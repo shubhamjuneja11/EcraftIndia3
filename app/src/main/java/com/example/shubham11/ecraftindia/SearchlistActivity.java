5e2cd83874b800812f3d1dd7d2c03d5c3c4f0a60
Shubham Juneja <shubhamjuneja11@gmail.com>
2017-05-28 03:03:44 +0530

    toolbar for search

--- app/src/main/java/com/example/shubham11/ecraftindia/SearchlistActivity.java
+++ app/src/main/java/com/example/shubham11/ecraftindia/SearchlistActivity.java
@@ -2,6 +2,7 @@ package com.example.shubham11.ecraftindia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class SearchlistActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class SearchlistActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searchlist);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 }

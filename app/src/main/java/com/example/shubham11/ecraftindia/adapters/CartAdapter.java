b81daadb7842bcc5a1e99cc013712b809c2fc6a2
Shubham <shubhamjuneja11@gmail.com>
2017-06-08 02:05:23 +0530

    working on cart

--- app/src/main/java/com/example/shubham11/ecraftindia/adapters/CartAdapter.java
+++ app/src/main/java/com/example/shubham11/ecraftindia/adapters/CartAdapter.java
@@ -1,8 +1,46 @@
 package com.example.shubham11.ecraftindia.adapters;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.shubham11.ecraftindia.R;
+import com.example.shubham11.ecraftindia.models.CartModel;
+
+import java.util.ArrayList;
+
 /**
  * Created by shubhamjuneja11 on 8/6/17.
  */
 
-public class CartAdapter {
+public class CartAdapter extends RecyclerView.Adapter<CartAdapter.MyViewHolder> {
+ArrayList<CartModel> al;
+    Context context;
+    public CartAdapter(Context context, ArrayList<CartModel>al){
+        this.context=context;
+        this.al=al;
+    }
+    @Override
+    public CartAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View item= LayoutInflater.from(parent.getContext()).inflate(R.layout.cart_row,parent,false);
+        return new MyViewHolder(item);
+    }
+
+    @Override
+    public void onBindViewHolder(CartAdapter.MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return al.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public MyViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
--- app/src/main/java/com/example/shubham11/ecraftindia/models/CartModel.java
+++ app/src/main/java/com/example/shubham11/ecraftindia/models/CartModel.java
@@ -5,4 +5,46 @@ package com.example.shubham11.ecraftindia.models;
  */
 
 public class CartModel {
+    String name,sku;
+    int cp,quantity;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public int getCp() {
+        return cp;
+    }
+
+    public void setCp(int cp) {
+        this.cp = cp;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public CartModel(String name, String sku, int cp, int quantity) {
+
+        this.name = name;
+        this.sku = sku;
+        this.cp = cp;
+        this.quantity = quantity;
+    }
 }
